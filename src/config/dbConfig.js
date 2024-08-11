0b9f50542260f74a5020da035be80f574d72fe1d
OshiVidu <vidurangib@uni.coventry.ac.uk>
2024-08-11 19:02:51 +0530

    Change dbConfig.js file according to the .env file

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config();
+dotenv.config({ path: './.env' });
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.DB_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             // useNewUrlParser: true,
             // useUnifiedTopology: true,
         });
