39d3137f96f867babcf6cc48162063e2c594caca
OshiVidu <vidurangib@uni.coventry.ac.uk>
2024-08-11 17:51:47 +0530

    Change Train.js file code structure

--- src/models/Train.js
+++ src/models/Train.js
@@ -1,14 +1,54 @@
 const mongoose = require('mongoose');
 
 const trainSchema = new mongoose.Schema({
-    train_id: String,
-    timestamp: Date,
-    latitude: Number,
-    longitude: Number,
-    speed: Number,
-    direction: String,
+ trainId: {
+ type: String,
+ required: true,
+ unique: true,
+ },
+ location: {
+ latitude: {
+ type: Number,
+ required: true,
+ },
+ longitude: {
+ type: Number,
+ required: true,
+ },
+ },
+ speed: {
+ type: Number,
+ required: true,
+ },
+ heading: {
+ type: String,
+ required: true,
+ },
+ timestamp: {
+ type: Number,
+ required: true,
+ },
 });
 
 const Train = mongoose.model('Train', trainSchema);
 
-module.exports = Train;
+module.exports = Train
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// const trainSchema = new mongoose.Schema({
+//     train_id: String,
+//     timestamp: Date,
+//     latitude: Number,
+//     longitude: Number,
+//     speed: Number,
+//     direction: String,
+// });
+
+// const Train = mongoose.model('Train', trainSchema);
+
+// module.exports = Train;
